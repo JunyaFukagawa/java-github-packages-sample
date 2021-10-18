bb04a620d6b57767a62d4920ff7f883206f973e1
fjunya <jfjfje@gmail.com>
2021-10-18 16:24:41 +0900

    no message

--- src/main/kotlin/net/phoneappli/githubpackagessample/Sample.kt
+++ src/main/kotlin/net/phoneappli/githubpackagessample/Sample.kt
@@ -1,7 +1,16 @@
 package net.phoneappli.githubpackagessample
 
+import com.fasterxml.jackson.databind.ObjectMapper
+
+
+
+
 class Sample {
-    fun printHelloWorld() {
-        println("Hello, world!")
+    fun printJson() {
+        val mapper = ObjectMapper()
+        val json = mapper.writeValueAsString(Hoge("Hello", "World"))
+        println(json)
     }
 }
+
+class Hoge(val value1: String, val Value2: String)
